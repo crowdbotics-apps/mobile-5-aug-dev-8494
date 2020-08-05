328d21c7b42c9c6ab748e7ae49c54a5ee2015554
Crowdbotics <team@crowdbotics.com>
2020-08-05 05:06:23 +0000

    Update models

--- backend/mobile_5_aug_dev_8494/wsgi.py
+++ backend/mobile_5_aug_dev_8494/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobile_5_aug_dev_8494.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mobile_5_aug_dev_8494.settings")
 
 application = get_wsgi_application()
